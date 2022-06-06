daa30e4be76dbcfa6466c6ea8209b9edab43c8e3
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2022-06-06 12:09:57 +0530

    Add files via upload

---
+++ pages/thankyou.js
@@ -0,0 +1,20 @@
+ 
+import Head from "next/head";
+import { Container } from "react-bootstrap";
+import { CMS_NAME } from "../lib/constants"; 
+ 
+
+export default function Thankyou( ) {
+   
+  return (
+    <>
+      <Head>
+        <title> {CMS_NAME} </title>
+      </Head> 
+       <Container>
+         <h4 className="my-5"> Thank you for your message we will get back to you as soon as possible. </h4>
+       </Container>
+    </>
+  );
+}
+ 
