9d29fcded7d495913a185596f7a0ce59c33aca50
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2022-06-09 12:06:02 +0530

    Delete components directory

--- components/tours/tour-title.js
+++
@@ -1,9 +0,0 @@
-import { Container } from "react-bootstrap";
-
-export default function TourTitle({ children }) {
-  return (
-    <Container>
-      <h1 className="mt-5 text-center">{children}</h1>
-    </Container>
-  );
-}
