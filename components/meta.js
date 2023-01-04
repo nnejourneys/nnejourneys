19729fcd03c5eeecdea0cc988e07536c5cc79eee
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2023-01-04 14:56:23 +0530

    Add files via upload

--- components/meta.js
+++ components/meta.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { CMS_NAME, HOME_OG_IMAGE_URL, EXAMPLE_PATH } from "../lib/constants";
+import { CMS_NAME, HOME_OG_IMAGE_URL, BASE_PATH } from "../lib/constants";
 
 export default function Meta() {
   return (
@@ -8,7 +8,7 @@ export default function Meta() {
       <meta name="theme-color" content="#000" />
 
       {/* General Meta */}
-      <meta property="og:url" content={`${EXAMPLE_PATH}`} />
+      <meta property="og:url" content={`${BASE_PATH}`} />
       <meta property="og:type" content="website" />
       <meta property="og:title" content={`${CMS_NAME}`} />
       <meta
@@ -22,7 +22,7 @@ export default function Meta() {
         name="twitter:card"
         content={`North by Northeast Journeys, specializes in the little-known Northeast region of India, offering adventure, nature and culture tours across Assam, Arunachal Pradesh, Meghalaya, Nagaland and Sikkim. | ${CMS_NAME}.`}
       />
-      <meta name="twitter:site" content={`${EXAMPLE_PATH}`} />
+      <meta name="twitter:site" content={`${BASE_PATH}`} />
       <meta name="twitter:creator" content="@mogsishere" />
     </Head>
   );
