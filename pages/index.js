a142475939bf04e3b42c094f961348990390b32e
nnejourneys <106916247+nnejourneys@users.noreply.github.com>
2023-01-19 10:50:00 +0530

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
 import Head from "next/head";
+import { getAllTours } from "../lib/api";
 import { CMS_NAME, imgblurDataURL, BASE_PATH,  HOME_OG_IMAGE_URL } from "../lib/constants";
 import About from "../components/about";
 import CTA from "../components/cta";
 import Testimonials from "../components/testimonials";
 import TourTypes from "../components/tour-types"; 
 import Region from "../components/region";
+import ToursSlider from "../components/tours-slider";
 
-export default function Index( ) {
+export default function Index({ allTours }) {
+
+  const departures = allTours.filter(
+    (tour) => tour.category == "departures"
+  );
    
   return (
     <>
@@ -16,10 +22,30 @@ export default function Index( ) {
         <meta name="robots" content="index"/>
       </Head> 
       <TourTypes />
+      <ToursSlider tours={departures}/>
       <About /> 
       <Region/>
       <CTA />
       <Testimonials /> 
+      
     </>
   );
-} 
+}
+
+
+export async function getStaticProps() {
+  const allTours = getAllTours([
+    'title',
+    'draft', 
+    'slug',
+    'subtitle',
+    'bg_image', 
+    'cat',
+    'category',
+    'type', 
+  ]);
+
+  return {
+    props: { allTours },
+  };
+}
