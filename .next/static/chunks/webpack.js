f470d7f8f741a26ba0e008acaa8674eacb26e216
nnejourneys <nnejourneys@gmail.com>
2024-01-04 13:57:58 +0530

    Added current year
    Tour display page slug previously said 'departures-vijaynagar-cycle-expedition-2023' | Changed to 'departures-vijaynagar-cycle-expedition
    
    Departure list page - Changed year from 2023 to 2024'

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a981d39747553204"; }
+/******/ 		__webpack_require__.h = function() { return "a4f3bac74baac8fc"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
---
+++ .next/static/webpack/pages/_app.afb1c48ba4f2b6a9.hot-update.js
@@ -0,0 +1,32 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdate_N_E"]("pages/_app",{
+
+/***/ "./data/alert.json":
+/*!*************************!*\
+  !*** ./data/alert.json ***!
+  \*************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"contact":[{"title":"email","icon":"ph:envelope","name":"nnejourneys@gmail.com","link":"mailto:nnejourneys@gmail.com","class":"list-inline-item mx-2 my-1"},{"title":"phone1","icon":"ph:device-mobile","name":"+91 7086855283","link":"tel:+917086855283","class":"list-inline-item mx-2 my-1"}],"social":[{"name":"facebook","icons":"bi:facebook","link":"https://www.facebook.com/northbynortheastjourneys"},{"name":"instagram","icons":"bi:instagram","link":"https://www.instagram.com/north_by_northeast/"},{"name":"instagram","icons":"bi:youtube","link":"https://www.youtube.com/channel/UCoOuUJl2OclF3xM2nCKAlSA/videos"}]}');
+
+/***/ }),
+
+/***/ "./data/heroslider.json":
+/*!******************************!*\
+  !*** ./data/heroslider.json ***!
+  \******************************/
+/***/ (function(module, __unused_webpack_exports, __webpack_require__) {
+
+module.exports = JSON.parse('{"hero":{"slides":[{"title":"North by Northeast Journeys","subtitle":"Discover the Other India","description":"Active, Interactive, Authentic Holidays in Northeast India","animation":"fadeInDown","delay":".4","image":"/images/hero-slider/cycling-trips-northeast-india.jpg"},{"title":"North by Northeast Journeys","subtitle":"Discover the Other India","description":"Active, Interactive, Authentic Holidays in Northeast India","animation":"fadeInDown","delay":".4","image":"/images/hero-slider/walking-holidays-northeast-india.jpg"},{"title":"North by Northeast Journeys","subtitle":"Discover the Other India","description":"Active, Interactive, Authentic Holidays in Northeast India","animation":"fadeInDown","delay":".4","image":"/images/hero-slider/motorcycle-trips-north-east-india.jpg"},{"title":"North by Northeast Journeys","subtitle":"Discover the Other India","description":"Active, Interactive, Authentic Holidays in Northeast India","animation":"fadeInDown","delay":".4","image":"/images/hero-slider/nature-culture-tours-in-northeast-india-ny-north-by-north-east-journeys.jpg"}]}}');
+
+/***/ })
+
+});
